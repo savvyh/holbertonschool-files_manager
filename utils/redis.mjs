88c2132b0d45c739d9f311f00db6d8bb44464ccd
savvyh <7277@holbertonstudents.com>
2025-09-23 12:49:21 +0000

    fix: apply npx eslint . --fix

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -3,7 +3,7 @@ import redis from 'redis';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    
+
     this.client.on('error', (error) => {
       console.error(`Redis client not connected to the server: ${error.message}`);
     });
