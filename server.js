63402cb2df1a21827d2a009c57d36108ce49c2c5
savvyh <7277@holbertonstudents.com>
2025-09-24 11:58:03 +0000

    fix: correct eslint

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 export default app;
