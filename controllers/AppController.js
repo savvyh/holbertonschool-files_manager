ff46fefc77c40179f1ff19710116541a707ee5b3
TuyenH <thtuyen199@gmail.com>
2025-09-24 11:44:56 +0000

    first API

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import redisClient from '../utils/redis.mjs';
+import dbClient from '../utils/db.mjs';
+
+class AppController {
+  static async getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    return res.status(200).json(status);
+  }
+
+  static async getStats(req, res) {
+    const [users, files] = await Promise.all([
+      dbClient.nbUsers(),
+      dbClient.nbFiles(),
+    ]);
+    return res.status(200).json({ users, files });
+  }
+}
+
+export default AppController;
