dddcab1d9169b9192c31607caf297f9d3bfe2aba
savvyh <7277@holbertonstudents.com>
2025-09-25 07:16:19 +0000

    feat: add new routes to connect, disconnect and get user base on token

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import FilesController from '../controllers/FilesController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 router.post('/users', UsersController.postNew);
-
 router.post('/files', FilesController.postUpload);
 
 export default router;
