893facd79a409ff811a635ab96dafff2be77c416
savvyh <7277@holbertonstudents.com>
2025-09-25 09:05:38 +0000

    feat: add routes to get files id

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,8 @@ router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 router.post('/users', UsersController.postNew);
 router.post('/files', FilesController.postUpload);
 
