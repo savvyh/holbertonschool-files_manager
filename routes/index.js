471267863493bfa4784ed1f126bd59d418470c4f
savvyh <7277@holbertonstudents.com>
2025-09-25 14:38:39 +0000

    refactor: reorganize routes

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,19 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
+
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
-router.post('/users', UsersController.postNew);
-router.post('/files', FilesController.postUpload);
 
 export default router;
